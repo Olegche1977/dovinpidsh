dfbb0cf9b9ee5563569f3e008c23a4a7f45af98e
Oleg <olegche1977@gmai.com>
2023-09-22 21:14:08 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Vinpidshypnic !!!</h2>
         </div>
+
+        <h2>Varya, Hellow !!!</h2>
        
       </div>
     );
